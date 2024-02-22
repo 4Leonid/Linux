ec2e3419049380d92152e50f3dc855cfbfc0e2d8
Leo <leo-turko@mail.ru>
2024-02-22 15:15:08 +0300

    Make all task fot Project One

--- SWIFT PROJECTS/HW1/ProjectOne/ProjectOne/ViewController.swift
+++ SWIFT PROJECTS/HW1/ProjectOne/ProjectOne/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
   private lazy var stackView: UIStackView = {
     let element = UIStackView()
     element.axis = .vertical
+    element.spacing = 10
+    element.distribution = .fillProportionally
     element.translatesAutoresizingMaskIntoConstraints = false
     return element
   }()
@@ -27,18 +29,36 @@ class ViewController: UIViewController {
   private lazy var authLabel: UILabel = {
     let element = UILabel()
     element.text = "Авторизация"
+    element.textAlignment = .center
     element.translatesAutoresizingMaskIntoConstraints = false
     return element
   }()
   
   private lazy var loginTextField: UITextField = {
     let element = UITextField()
+    element.backgroundColor = .green
+    element.layer.borderColor = UIColor.darkGray.cgColor
+    element.borderStyle = .roundedRect
+    element.placeholder = "Login"
     element.translatesAutoresizingMaskIntoConstraints = false
     return element
   }()
   
   private lazy var passwordTextField: UITextField = {
     let element = UITextField()
+    element.backgroundColor = .yellow
+    element.borderStyle = .roundedRect
+    element.placeholder = "Password"
+    element.translatesAutoresizingMaskIntoConstraints = false
+    return element
+  }()
+  
+  private lazy var enterButton: UIButton = {
+    let element = UIButton(type: .system)
+    element.setTitle("Войти", for: .normal)
+    element.backgroundColor = .blue
+    element.layer.cornerRadius = 16
+    element.tintColor = .white
     element.translatesAutoresizingMaskIntoConstraints = false
     return element
   }()
@@ -46,6 +66,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     setViews()
+    setConstraints()
   }
 }
 
@@ -58,14 +79,22 @@ private extension ViewController {
     stackView.addArrangedSubview(authLabel)
     stackView.addArrangedSubview(loginTextField)
     stackView.addArrangedSubview(passwordTextField)
-    
+    stackView.addArrangedSubview(enterButton)
   }
   
   func setConstraints() {
     NSLayoutConstraint.activate([
       stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-      stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+      stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+      stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+      
+      image.widthAnchor.constraint(equalTo: stackView.widthAnchor),
+      image.heightAnchor.constraint(equalTo: image.widthAnchor),
+     
+      authLabel.heightAnchor.constraint(equalToConstant: 20),
+      loginTextField.heightAnchor.constraint(equalToConstant: 40),
+      passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+      enterButton.heightAnchor.constraint(equalToConstant: 50),
     ])
   }
 }
