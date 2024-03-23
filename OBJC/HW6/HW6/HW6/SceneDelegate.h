01991c4fa95767d5cc8c2d4fcce64098cc45a1d2
Leo <leo-turko@mail.ru>
2024-03-23 19:22:57 +0300

    Make homework 6

---
+++ OBJC/HW6/HW6/HW6/SceneDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SceneDelegate.h
+//  HW6
+//
+//  Created by Леонид Турко on 22.03.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
---
+++ OBJC/HW6/HW6/HW6/ViewController.h
@@ -0,0 +1,12 @@
+//
+//  ViewController.h
+//  HW6
+//
+//  Created by Леонид Турко on 22.03.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
